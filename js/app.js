d175b30581090a26990cc1933800d2cd26bfe447
Olea Sebastian <oleasmulovitz.sebastian@iesconcepcion.edu.ar>
2023-09-18 20:19:40 -0300

    Meteo app con img

--- js/app.js
+++ js/app.js
@@ -2,7 +2,13 @@ const Btn = document.getElementById("Btn")
 const Ciudad = document.getElementById("Ciudad");
 const Clima = document.getElementById("Clima");
 const Temp= document.getElementById("Temp");
-const
+const Max= document.getElementById("Max");
+const Min= document.getElementById("min");
+const Viento= document.getElementById("Viento");
+const Humedad= document.getElementById("Humedad")
+const imagen= document.getElementById("imagen");
+
+
 
 Btn.addEventListener('click', (e) => {
     event.preventDefault(e)
@@ -19,5 +25,40 @@ Btn.addEventListener('click', (e) => {
          console.log("bien capo")
          Ciudad.innerHTML=`${json.name}, ${json.sys.country}`;
          Temp.innerHTML=`${parseInt(json.main.temp)}<span>°C</span>`;
+         Max.innerHTML = `${parseInt(json.main.temp_max)}`;
+         Min.innerHTML=`${parseInt(json.main.temp_min)}`
+         Viento.innerHTML =`${parseInt(json.wind.speed)}Km/h`;
+         Humedad.innerHTML=`${(json.main.humidity)}%`
+
+
+         switch (json.weather[0].main) {
+            case 'Clear':
+                
+                imagen.src = './img/clear.png';
+                break;
+
+            case 'Rain':
+              
+                imagen.src = 'img/rain.png';
+                break;
+
+            case 'Snow':
+               
+                imagen.src = "img/snow.png";
+                break;
+
+            case 'Clouds':
+                
+                imagen.src = 'img/cloud.png';
+                break;
+
+            case 'Haze':
+               
+                imagen.src = 'img/mist.png';
+                break;
+
+            default:
+                imagen.src = 'img/world.gif';
+        }
     })
 })
