87703b50bb301a59d56502ef35cb1bb8c80fe7c9
Olea Sebastian <oleasmulovitz.sebastian@iesconcepcion.edu.ar>
2023-09-18 20:43:44 -0300

    agregado style error404

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ const Min= document.getElementById("min");
 const Viento= document.getElementById("Viento");
 const Humedad= document.getElementById("Humedad")
 const imagen= document.getElementById("imagen");
-
+const caja= document.getElementById("Caja")
 
 
 Btn.addEventListener('click', (e) => {
@@ -19,9 +19,13 @@ Btn.addEventListener('click', (e) => {
     
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${Buscar}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json => {
         if (json.cod === "404") {
+            imagen.src= "./img/404.png"
             console.log("ERROR CAPO")
+            caja.style.display="none"
+            
             return;
         }
+        caja.style.display="block"
          console.log("bien capo")
          Ciudad.innerHTML=`${json.name}, ${json.sys.country}`;
          Temp.innerHTML=`${parseInt(json.main.temp)}<span>°C</span>`;
